ff27165cef0af3bc493aad80c4499cff55ea3856
testlnord <testlnord@gmail.com>
2013-11-09 12:03:23 +0400

    Added dataset marker

--- csvviewer.py
+++ csvviewer.py
@@ -18,19 +18,35 @@ def picture_iterator(path = './training.csv'):
     pass
 
 if __name__ == "__main__":
+    """
+    for pic in picture_iterator():
+        for dot in pic.keys():
+            if dot != 'Image':
+                cv2.circle(pic['Image'], (int(float(pic[ dot[:-1]+'x'])),int(float(pic[ dot[:-1]+'y']))),2,(255,) )
+        cv2.imshow('img',pic['Image'])
+        cv2.waitKey(1000)
+    """
     with open('result.csv','w') as result_file:
         dw = csv.DictWriter(result_file, ('emotion', 'number'))
         pic_num = 0;
-        emotions = {'страх': 0, 'радость': 1, 'грусть': '3'}
+
+        emotions = {'страх': 0, 'радость': 1, 'печаль': 2, 'гнев': 3, 'Удивление':4 , 'Отвращение':5, 'удовольствие':6}
+        for em in emotions:
+            print em,' => ', emotions[em]
         for pic in picture_iterator():
             cv2.imshow('e2', pic['Image'])
             descriptor = {'number': pic_num}
-            descriptor['emotion'] = emotions['страх']
-            dw.writerow(descriptor)
-
             c = cv2.waitKey(-1)
-            if c== 1048603:
+            if c == 1048603:
                 break
+            descriptor['emotion'] = {
+                1048625: 0, #1 - страх
+                1048626: 1, #2 - радость
+                1048627: 2, #3 - печаль
+                1048628: 3, #4 - гнев
+                1048629: 4, #5 - удивление
+                1048630: 5, #6 - отвращение
+                1048631: 6  #7 - удовольствие
+            }[c]
+            dw.writerow(descriptor)
             pic_num+=1
-            if pic_num == 4:
-                break
