a54e4cb2656adc6a80015872d366820686c69e5b
testlnord <testlnord@gmail.com>
2013-11-16 18:16:21 +0400

    some refactoring

--- boosting_classifier.py
+++ boosting_classifier.py
@@ -6,13 +6,13 @@ import numpy as np
 import csvviewer
 import csv_merger
 
-def train(data_path='Data/merged_data.csv'):
+def train(data_path='Data/merged_data.csv', emotion=1):
     data = []
     with open(data_path,'r') as data_file:
         for data_line in data_file:
             data.append([float(value) for value in data_line.split(',')])
     data = np.float32(np.array(data))
-    smile_responses = [1 if value == 1 else 0 for value in data[:,1]]
+    smile_responses = [1 if value == emotion else 0 for value in data[:,1]]
     smile_responses = np.float32(np.array(smile_responses))
     print data[:,1].shape
     booster = cv2.Boost(trainData=data[:, 2:],tflag=cv2.CV_ROW_SAMPLE, responses=smile_responses)
@@ -20,7 +20,7 @@ def train(data_path='Data/merged_data.csv'):
     booster.save("Data/boost_emotions_classifier.xml")
 
 
-def classify(path_to_classifier_xml = "Data/boost_emotions_classifier.xml"):
+def classify(path_to_classifier_xml = "Data/boost_emotions_classifier.xml",show_only_good = False):
     booster = cv2.Boost()
     booster.load(path_to_classifier_xml)
 
@@ -33,9 +33,11 @@ def classify(path_to_classifier_xml = "Data/boost_emotions_classifier.xml"):
         result = booster.predict(distances)
         copy = np.zeros((96, 192), dtype=np.uint8)
         copy[:, :96] = picture_collection[int(pic['number'])]['Image'][:,:]
-
+        if not show_only_good :
+            if result < 1:
+                continue
         cv2.putText(img=copy,
-                    text="smile" if result > 0 else "not smile",
+                    text="found" if result > 0 else "not found",
                     org=(96, 48),
                     fontFace=cv2.FONT_HERSHEY_COMPLEX,
                     fontScale=0.8,
@@ -46,4 +48,5 @@ def classify(path_to_classifier_xml = "Data/boost_emotions_classifier.xml"):
 
 
 if __name__ == "__main__":
+    train(emotion=4)
     classify()
