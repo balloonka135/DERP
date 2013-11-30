a8a349aec3fb3fce8753a6b1b98f352efcea0ea6
SketchTurner <mih.dmitry@gmail.com>
2013-11-30 17:41:51 +0400

    (todo validate)

--- svm_classify.py
+++ svm_classify.py
@@ -7,18 +7,26 @@ from sklearn import svm
 import numpy
 import cPickle as cpkl
 
-input_clf = open("./trained_data/trained_classifier.pkl","rb")
-input_obj = open("./trained_data/objects.pkl","rb")
+input_clf = open("./Data/trained_data/trained_classifier.pkl", "rb")
+input_obj = open("./Data/trained_data/objects.pkl", "rb")
+input_cls = open("./Data/trained_data/classes.pkl", "rb")
 classifier = cpkl.load(input_clf)
 objects = cpkl.load(input_obj)
-#todo :сделать классификацию последних 5% объектов и красиво вывести 
-#картинка : эмоция 
+classes = cpkl.load(input_cls)
+
+#dec_func = classifier.decision_function(objects[2100])
+#print("predicted data:{0}".format(classifier.predict(objects[2104])))
+#print("real emotion: {0}".format(classes[2104]))
+
 result = classifier.predict(objects[2104])
 
+'''
 if result == 1:
 	print "joyness"
 else:
 	print "not joyness"
+'''
 
 input_clf.close()
 input_obj.close()
+input_cls.close()
--- svm_train.py
+++ svm_train.py
@@ -4,16 +4,12 @@
 import csv
 import cv2
 from sklearn import svm
+from sklearn import cross_validation as cv
 import numpy
 import cPickle as cpkl
 import os
 import shutil
 import glob
-
-# В качестве объекта берутся расстояния между ключевыми точками на изображении
-objects=[]
-#Классы от 0 до 7
-classes=[]
 
 FEARNESS = 0 #страх
 JOYNESS = 1 #радость
@@ -24,52 +20,94 @@ DISGUST = 5 #отвращение
 PLEASURE = 6 #удовольствие
 NEUTRAL = 7 #нейтральный
 
-temp=[]
-#парсинг файла для тренировки классификатора
-with open('merged_data.csv', 'rb') as fd:
-	#читаем данные в список(отдельный список для каждого изображения):
-	#['номер картинки', 'эмоция', 'расстояние1',...,'расстояние105']
-	training_data = csv.reader(fd)
-	#бежим по каждому списку
-	for data in training_data:
-		#в список classes кладем эмоции 
-		classes.append(int(data[1]))
-		#бежим по расстояниям и кладем их в отдельный список
-		#далее мы будем тренировать классфикатор, по данным объектам
-		#элементу из списка classes соответствует элемент-список из списка objects
-		for i in range(2,107):
-			temp.append(float(data[i]))
-		objects.append(temp)
-		temp=list()
+# В качестве объекта берутся расстояния между ключевыми точками на изображении
+#Классы от 0 до 7
+classes = []
+objects = []
+## Данные для тестирования
+test_classes = []
+test_objects = []
 
-#данные получены, теперь можно делать с ними все захочется
-#будем выполнять бинарную классификацию
-#классификатор будет определять эмоцию радость на изображении
-#сделаем cross-validation
+train_data_path = "./Data/dataset/merged_data_train.csv"
+val_data_path = "./Data/dataset/merged_data_val.csv"
+test_data_path = "./Data/dataset/merged_data_test.csv"
 
-class_joyness_or_not=[1 if i==JOYNESS  else 0 for i in classes]
-#заводим объект-классификатор SVM
-classifier = svm.LinearSVC()
-#95% данных для тренировки (2033)
-crossval_datalen = int(len(objects)*95/100)
-#5% оставшихся данных для тестирования (107)
-test_datalen = len(objects)-crossval_datalen   
-#тренируем классификатор на 95% данных
-#пока что просто fit, далее надо читать про мультиклассовую классификацию
-classifier.fit(objects[:crossval_datalen], class_joyness_or_not[:crossval_datalen])
+#читает данные в список вида:
+#['номер картинки', 'эмоция', 'расстояние1',...,'расстояние105']
+#в список classes кладем эмоции
+#в список objects кладем объекты
+#бежим по расстояниям и кладем их в список objects
+#далее мы будем тренировать классфикатор, по данным объектам
+#элементу из списка classes соответствует элемент-список из списка objects
+class DataMaker(object):
+    def __init__(self, path):
+        self.path = path
+        self.train_classes = []
+        self.train_objects = []
+        self.temp = []
+
+    def get_classes(self):
+        with open(self.path, 'rb') as fd:
+            for data in fd:
+                data = list(data.split(", "))
+                self.train_classes.append(int(data[1]))
+        return self.train_classes
+
+    def get_objects(self):
+        with open(self.path, 'rb') as fd:
+            for data in fd:
+                data = list(data.split(", "))
+                for i in range(2, 107):
+                    self.temp.append(float(data[i]))
+                self.train_objects.append(self.temp)
+                self.temp = list()
+        return self.train_objects
+
+## Далее применим 3-way-cross-validation (60-20-20)
+#Читаем данные для тренировки
+train_data_reader = DataMaker(train_data_path)
+classes = train_data_reader.get_classes()
+objects = train_data_reader.get_objects()
+
+#тренируем классификатор
+classifier = svm.SVC(kernel='linear')
+classifier.fit(objects, classes)
+
+classes = list()
+objects = list()
+#читаем данные для кросс валидации
+val_data_reader = DataMaker(val_data_path)
+classes = val_data_reader.get_classes()
+objects = val_data_reader.get_objects()
+
+####
+######### здесь будет cross - validation tests
+####
+
+#читаем данные для тестирования
+#(далее передадим их в качестве объектов для файла svm_classify.py)
+test_data_reader = DataMaker(test_data_path)
+test_classes = test_data_reader.get_classes()
+test_objects = test_data_reader.get_objects()
 
 #сохраняем тренированный объект-классификатор в файл
-#и объекты чтобы не считывать снова csv - файл а сразу провести тест
-#на имеющихся данных
-newpath = r"./trained_data/"
+#а также сохраняем объекты на которых будем тестировать классификатор
+#еще для сравнения сохраняем объекты
+
+newpath = r"./Data/trained_data_svm/"
 if not os.path.exists(newpath):
-	os.makedirs(newpath)
-output_clf = open(r"./trained_data/trained_classifier.pkl","wb")
-output_obj = open(r"./trained_data/objects.pkl","wb")
+    os.makedirs(newpath)
+
+output_clf = open(r"./Data/trained_data_svm/trained_classifier.pkl", "wb")
+output_obj = open(r"./Data/trained_data_svm/objects.pkl", "wb")
+output_cls = open(r"./Data/trained_data_svm/classes.pkl", "wb")
+
 cpkl.dump(classifier, output_clf)
-cpkl.dump(objects, output_obj)
+cpkl.dump(test_objects, output_obj)
+cpkl.dump(test_classes, output_cls)
+
 output_clf.close()
 output_obj.close()
-
+output_cls.close()
 
 
