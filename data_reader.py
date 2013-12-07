54bef3b6359826af0865baf7f5510ca05a45d670
SketchTurner <mih.dmitry@gmail.com>
2013-12-07 13:58:33 +0400

    dataset reader

---
+++ data_reader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+#coding=utf-8
+
+#auhtor = sketchturner
+#dataset reader
+
+class DataReader(object):
+    def __init__(self, path):
+        self.path = path
+
+    def read_generator(self):
+        with open(self.path, 'rb') as fd:
+            for data in fd:
+                data = data.split(", ")
+                yield data
+
+    def get_classes(self):
+        train_classes = []
+        for data in self.read_generator():
+            train_classes.append(int(data[1]))
+        return train_classes
+
+    def get_objects(self):
+        train_objects = []
+        temp = []
+        for data in self.read_generator():
+            temp=data[2:107]
+            train_objects.append(temp)
+            temp = list()
+        return train_objects
+
--- svm_train.py
+++ svm_train.py
@@ -39,48 +39,48 @@ train_data_path = "./Data/dataset/merged_data_train.csv"
 val_data_path = "./Data/dataset/merged_data_val.csv"
 test_data_path = "./Data/dataset/merged_data_test.csv"
 
-#читает данные в список вида:
-#['номер картинки', 'эмоция', 'расстояние1',...,'расстояние105']
-#в список classes кладем эмоции
-#в список objects кладем объекты
-#бежим по расстояниям и кладем их в список objects
-#далее мы будем тренировать классфикатор, по данным объектам
-#элементу из списка classes соответствует элемент-список из списка objects
-class DataMaker(object):
-    def __init__(self, path):
-        self.path = path
-        self.train_classes = []
-        self.train_objects = []
-        self.temp = []
 
-    def get_classes(self):
-        with open(self.path, 'rb') as fd:
-            for data in fd:
-                data = list(data.split(", "))
-                self.train_classes.append(int(data[1]))
-        return self.train_classes
 
-    def get_objects(self):
-        with open(self.path, 'rb') as fd:
-            for data in fd:
-                data = list(data.split(", "))
-                for i in range(2, 107):
-                    self.temp.append(float(data[i]))
-                self.train_objects.append(self.temp)
-                self.temp = list()
-        return self.train_objects
 
-##estimate classifier error
-def evaluate_error(objects, classes, clf):
-    err_count = 0
-    for i in range(len(objects)):
-        pr = clf.predict(objects[i])
-        print("real:{0}\n predicted:{1}\n".format(classes[i], pr))
-        if classes[i] != pr:
-            err_count += 1
-    err_percentage = (err_count*100)/len(objects)
-    print("errors:{0}, labels:{1}\n".format(err_count, len(objects)))
-    print("errors percentage:{0} %\n".format(err_percentage))
+class SVMTrainer(object):
+    def __init__(self, clf, X_train, Y_train, X_val, Y_val):
+        self.X_train = X_train
+        self.Y_train = Y_train
+        self.X_val = X_val
+        self.Y_val = Y_val
+        self.clf = clf
+##оптимизация классификатора
+def eval_estimator(self, X_train, Y_train, X_val, Y_val):
+    #### tuned parameters
+    self.C = [i for i in range(1, 100)]
+    self.intercept_scaling=[i for i in range (1, 10)]
+    self.tol = [0.001, 0.00001]#, 0.00001, 0.00001]
+    self.parameters = [{'C':self.C,
+              'class_weight':['auto', None],
+              'intercept_scaling':self.intercept_scaling,
+              'tol':self.tol,
+              'dual':[False, True]}]
+    ####
+
+    self.scores = ['precision']##['accuracy', 'average_precision', 'precision', 'recall', 'f1', 'roc_auc']
+    for score in self.scores:
+        print("Tuning hyper-parameters for {0}\n".format(score))
+        clf=GridSearchCV(svm.LinearSVC(), parameters, cv=cv, scoring=score)
+        clf.fit(objects, classes)
+
+        print("Best parameters (with train set:\n")
+        print(clf.best_estimator_)
+        print("Grid scores (with train set):\n")
+        for params, mean_score, scores in clf.grid_scores_:
+            print("%0.5f (+/-%0.05f) for %r"
+                  % (mean_score, scores.std() / 2, params))
+        print("Detailed classification report:\n")
+        print("The model is trained on the full train set.\n")
+        print("The scores are computed on the full validation set.\n")
+        print("\n")
+        real_classes, pred_classes = val_classes, clf.predict(val_objects)
+        print(classification_report(real_classes, pred_classes))
+
 
 #Читаем все необходимые данные
 train_data_reader = DataMaker(train_data_path)
@@ -98,42 +98,19 @@ test_objects = test_data_reader.get_objects()
 #for binary classification joy and non joy classes
 #joy_cls = [1 if i == 1 else 0 for i in classes]
 
-
-#clf = svm.LinearSVC()
+clf = svm.LinearSVC()
 #clf.fit(objects, classes)
 #print("error percentage (underfitting check)\n:{0}%".format((1.0-clf.score(objects, classes))*100.))
 #print("error percentage (overfitting check)\n:{0}%".format((1.0-clf.score(test_objects, test_classes))*100.))
 
 # tuned parameters
-C = [i for i in range(1, 10)]
-intercept_scaling=[i for i in range (1, 3)]
-tol = [0.001,0.0001]#,0.00001,0.00001]
-
+intercept_scaling=[i for i in range (1, 10)]
+tol = [0.001,0.00001]#,0.00001,0.00001]
 parameters = [{'C':C,
-             # 'loss':['l2'],
-              #'penalty':['l2'],
               'class_weight':['auto',None],
               'intercept_scaling':intercept_scaling,
               'tol':tol,
               'dual':[False,True]}]
 
-scores = ['precision']##['accuracy', 'average_precision', 'precision', 'recall', 'f1', 'roc_auc']
 
-for score in scores:
-    print("Tuning hyper-parameters for {0}\n".format(score))
-    clf=GridSearchCV(svm.LinearSVC(), parameters, cv=3, scoring=score)
-    clf.fit(objects, classes)
-
-    print("Best parameters (with train set:\n")
-    print(clf.best_estimator_)
-    print("Grid scores (with train set):\n")
-    for params, mean_score, scores in clf.grid_scores_:
-        print("%0.5f (+/-%0.05f) for %r"
-              % (mean_score, scores.std() / 2, params))
-    print("Detailed classification report:\n")
-    print("The model is trained on the full train set.\n")
-    print("The scores are computed on the full validation set.\n")
-    print("\n")
-    real_classes, pred_classes = val_classes,clf.predict(val_objects)
-    print(classification_report(real_classes, pred_classes))
 
